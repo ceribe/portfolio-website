80fa759b2fe41cf0dd066577644dfc9b64f09cba
Ceribe <ceribekagami@gmail.com>
2022-04-07 23:06:05 +0200

    Add portfolio-website subpage

--- src/routes/projects/portfolio-website.svelte
+++ src/routes/projects/portfolio-website.svelte
@@ -1,5 +1,26 @@
 <script>
+	import Divider from '$lib/divider.svelte';
 	import Section from '$lib/section.svelte';
+	import TechnologyCard from '$lib/technologyCard.svelte';
 </script>
 
-<Section title={'Ta strona'} />
+<Section title={'Ta strona'}>
+	<div class="text-lg mb-12">
+		Statyczna strona internetowa dzięki, której informacje o mnie są zebrane w jednym łatwo
+		dostępnym miejscu. Głownym powodem stworzenia tej strony była chęć nauczenia się Svelte oraz
+		rozszerzenia swoich umiejętności o podstawy webdev-u. Wybrałem Svelte ze względu na prostotę
+		użycia oraz fakt, że możliwe jest generowanie z jego pomocą statycznych stron.
+	</div>
+
+	<Divider>Użyte techonologie</Divider>
+	<div class="w-full flex flex-wrap">
+		<TechnologyCard name="typescript" />
+		<TechnologyCard name="svelte" />
+		<TechnologyCard name="svelte_kit" />
+		<TechnologyCard name="tailwind_css" />
+		<TechnologyCard name="vscode" />
+		<TechnologyCard name="gitkraken" />
+		<TechnologyCard name="git" />
+		<TechnologyCard name="github" />
+	</div>
+</Section>

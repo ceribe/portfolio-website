5a400dbb526c82506ad6fc6369acf2ceb7003d60
ceribe <ceribekagami@gmail.com>
2022-04-08 20:18:16 +0200

    Change project card's size

--- src/lib/projectCard.svelte
+++ src/lib/projectCard.svelte
@@ -11,7 +11,7 @@
 	class="bg-[#f7f7f7] hover:bg-[#dddddd] rounded-2xl w-[26rem] h-[280px] flex justify-center items-center flex-col mr-4 mb-6 hover:shadow-xl drop-shadow-xl"
 	on:click={() => goto(link)}
 >
-	<img src={filename} alt={name} class="w-full h-full rounded-t-2xl" />
+	<img src={filename} alt={name} class="w-[450px] h-[300px] rounded-t-2xl" />
 	<div
 		class="font-bold text-xs border-t-2 border-[#bbbbbb] w-full h-full flex justify-center items-center p-2"
 	>

d1377ae907cfee9521a1b7219d9d2a11fb4557bd
ceribe <ceribekagami@gmail.com>
2022-04-10 12:35:28 +0200

    Update about section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,10 @@
 	<Divider>Wykształcenie</Divider>
 	<p class="text-base">Inżynier - Politechnika Poznańska (2022)</p>
 	<Divider>Doświadczenie</Divider>
+	<p class="text-base">
+		[2019 - Teraz] Prowadzenie sklepu wysyłkowego przez platformę
+		<a href="https://www.cardmarket.com/en/Magic/Users/Ceribe" class="text-blue-600">CardMarket</a>
+	</p>
 	<p class="text-base">[2019 - Teraz] Programista C++ (Frontend) - CAD Projekt K&A Sp. z o.o.</p>
 	<Divider>Kontakt</Divider>
 	<a href="mailto:szymon.opielewicz@gmail.com" class="text-blue-600 text-base">
